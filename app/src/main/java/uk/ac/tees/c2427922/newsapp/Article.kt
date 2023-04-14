62f3921e4199379cb3c16bc0e35e2b561892f0d6
Khan <work.s4rt7@simplelogin.com>
2023-04-14 16:58:53 +0100

    DAO and Article Setup Room

--- app/src/main/java/uk/ac/tees/c2427922/newsapp/Article.kt
+++ app/src/main/java/uk/ac/tees/c2427922/newsapp/Article.kt
@@ -1,9 +1,21 @@
 package uk.ac.tees.c2427922.newsapp
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
+
+@Entity(
+    tableName = "articles"
+)
 data class Article(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = null,
     val author: String,
+    val content: String,
+    val description: String,
     val publishedAt: String,
     val source: Source,
     val title: String,
-    val url: String
+    val url: String,
+    val urlToImage: String
 )
