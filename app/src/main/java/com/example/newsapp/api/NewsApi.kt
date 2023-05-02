b68ae4acc9f7436977cc7f4de6b84592ec927a4c
Khan <work.s4rt7@simplelogin.com>
2023-05-02 16:44:35 +0100

    search news fragment bug fixed

--- app/src/main/java/com/example/newsapp/api/NewsApi.kt
+++ app/src/main/java/com/example/newsapp/api/NewsApi.kt
@@ -9,6 +9,7 @@ import retrofit2.http.Query
 interface NewsApi {
     @GET("v2/top-headlines")
     suspend fun getBreakingNews(
+
         @Query("country")
         countryCode: String = "uk",
         @Query("page")
